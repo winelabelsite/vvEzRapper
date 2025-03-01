1d5f7ab1b4e08e832fe8a458c39197222db8b478
nuumanP <wlsjapan@yahoo.co.jp>
2025-03-01 21:32:40 +0900

    とりあえず意図した通りには動いている模様バージョン。

--- testlab/voicevox.py
+++ testlab/voicevox.py
@@ -1,131 +1,109 @@
 import json
-import inspect
+import AccentPhraseApps as APA
+import wavhandler as WH
+import accessEngine as AE
+
+BPM = 120
+
+def calc_ratio(bpm, wave_length, moras_count):
+    """bpmとwave_lengthから、1モーラあたりの秒数を計算し、それをbpmから求めた値と比較して、比率を求める"""
+    spb_given = 60 / bpm / 4
+    spb_before = wave_length / moras_count
+    ratio = spb_given / spb_before
+    print(f'spb_given = {spb_given:.6f}, spb_before = {spb_before:.6f}, ratio = {ratio:.6f}')
+    return ratio
+
+def calc_expected_length(bpm, moras_count):
+    """bpmとモーラ数から、期待される音声の長さを計算する"""
+    expected_length = 60 * moras_count / bpm / 4
+    return expected_length
+
+def make_sample(text, is_kana=False, wavefilename=None):
+        """テキストから音声波形を生成し、その長さとクエリを返す"""
+        ae = AE.AccessEngine()
+        wh = WH.WaveHandler()
+        query = ae.audio_query(text)
+        if is_kana is True:
+            accent_phrases = ae.accent_phrases(text, is_kana=is_kana)
+            query['accentPhrases'] = accent_phrases
+            query['kana'] = text
+        wave = ae.synthesis(query)
+        length_before = wh.get_length(wave)
+        if wavefilename is not None:
+            wh.write(wavefilename, wave)
+        return length_before, query 
+
+def set_average_length(query, wavefilename=None):
+        """クエリの各モーラの長さを平均化し、そのクエリを返す"""
+        ae = AE.AccessEngine()
+        wh = WH.WaveHandler()
+        average_length = APA.APLengthAverageCalcurator().run(query)
+        APA.APMoraLengthAdjuster(average_length).run(query)
+        APA.APDumper().run(query)
+        wave = ae.synthesis(query)
+        length = wh.get_length(wave)
+        if wavefilename is not None:
+            wh.write(wavefilename, wave)
+        return length, query
+
+def make_wavefile_from_query(query, wavefilename=None):
+    """クエリから音声波形を生成し、その長さを返す"""
+    ae = AE.AccessEngine()
+    wh = WH.WaveHandler()
+    wave = ae.synthesis(query)
+    length = wh.get_length(wave)
+    if wavefilename is not None:
+        wh.write(wavefilename, wave)
+    return length
 
 
-class AccentPhraseHandler:
-    """
-    実行する関数は子クラスが上書き
-    """
+def trial_algo01(text):
+    # print('お試し01 サンプル作って全体の長さ調整')
 
-    def __init__(self):
-        self.mora_count = 0
-        self.pause_mora_count = 0
+    # サンプル作って長さ測ってみる
+    length_before, query = make_sample(text, wavefilename='algo01_before.wav')
+    moras_count = APA.APMorasCounter().run(query)
+    ratio = calc_ratio(BPM, length_before, moras_count)
 
-    def preprocess(self, accent_phrases):
-        return None
+    # Queryを操作する
+    query['prePhonemeLength'] = 0.0
+    query['postPhonemeLength'] = 0.0
+    query["speedScale"] = 1 / ratio
+    query['outputSamplingRate'] = 48000
 
-    def mora_handler(self, mora):
-        raise NotImplementedError(
-            f"Subclasses should implement {inspect.currentframe().f_code.co_name} method")
-
-    def pause_mora_handler(self, pause_mora):
-        raise NotImplementedError(
-            f"Subclasses should implement {inspect.currentframe().f_code.co_name} method")
-
-    def moras_handler(self, moras):
-        return None
-
-    def accent_phrase_handler(self, accent_phrase):
-        return None
-
-    def generate_result(self, accent_phrases):
-        return None
-
-    def run_accent_phrase(self, accent_phrase):
-        moras = accent_phrase['moras']
-        for mora in moras:
-            self.mora_count += 1
-            self.mora_handler(mora)
-        self.moras_handler(moras)
-        if 'pauseMora' in accent_phrase:
-            self.pause_mora_handler(accent_phrase['pauseMora'])
-            self.pause_mora_count += 1
-        return
-
-    def run(self, accent_phrases):
-        self.preprocess(accent_phrases)
-        for ap in accent_phrases:
-            self.run_accent_phrase(ap)
-        return self.generate_result(accent_phrases)
+    # waveファイルを作成
+    length_after = make_wavefile_from_query(query, wavefilename='algo01_after.wav')
+    expected_length = calc_expected_length(BPM, moras_count)
+    difference = length_after - expected_length
+    # これ一つにまとめて使いまわしたいよね。
+    # print(f'moras_count = {moras_count}')   # queryから出せる
+    # print(f'length_before = {length_before:.6f}') # make_sampleから出せる
+    # print(f'ratio = {ratio:.6f}') # 上二つがわかれば出せる 
+    # print(f'length_after = {length_after:.6f}')   # 長さいじった後なら出せる
+    # print(f'expected_length = {expected_length:.6f}') # すぐ出せる
+    # print(f'difference = {difference}')   # すぐ出せる
+    # print(json.dumps(query, indent=2, ensure_ascii=False))    # 気になったら表示
+    print(expected_length / difference)
+    pass
 
 
-class APDumper(AccentPhraseHandler):
+if __name__ == "__main__":
+    texts01 = [
+    'じゅ',
+    'じゅげ',
+    'じゅげむー',
+    'じゅげむーじゅげむー',
+    'じゅげむーじゅげむーごこおのすりきれ',
+    'じゅげむーじゅげむーごこおのすりきれじゅげむーじゅげむー',
+    'じゅげむーじゅげむーごこおのすりきれじゅげむーじゅげむーごこおのすりきれ',
+    ]
 
-    def mora_handler(self, mora):
-        text = mora.get('text', '')
-        c = mora.get('consonantLength', 0)
-        v = mora.get('vowelLength', 0)
-        print(f'text = {text}, c = {c:.6f}, v = {v:.6f}, c + v = {c + v:.6f}')
+    texts02 = ['じぇじぇじぇじぇ' * i for i in range(1, 10)]
 
-    def pause_mora_handler(self, mora):
-        self.mora_handler(mora)
+    texts = texts02 + texts01
 
+    for text in texts[-1:]:
+        trial_algo01(text)
 
-class APLengthCalcrator(AccentPhraseHandler):
-    def __init__(self):
-        super().__init__()  # 親クラスのコンストラクタを呼び出す
-        self.total_length = 0
-
-    def mora_handler(self, mora):
-        c = mora.get('consonantLength', 0)
-        v = mora.get('vowelLength', 0)
-        length = c + v
-        self.total_length += length
-
-    def pause_mora_handler(self, pause_mora):
-        self.mora_handler(pause_mora)
-
-    def generate_result(self, accent_phrases):
-        return self.total_length
-
-
-class APLengthAverageCalcurator(APLengthCalcrator):
-    def generate_result(self, _):
-        average = self.total_length / (self.mora_count + self.pause_mora_count)
-        return average
-
-
-class APMorasCounter(APLengthCalcrator):
-    def generate_result(self, _):
-        return self.mora_count + self.pause_mora_count
-
-
-class APMorasCounter(APLengthCalcrator):
-    def generate_result(self, _):
-        return self.mora_count + self.pause_mora_count
-
-
-class APMoraLengthAdjuster(AccentPhraseHandler):
-    def preprocess(self, accent_phrases):
-        self.average_length = APLengthAverageCalcurator().run(accent_phrases)
-
-    def mora_handler(self, mora):
-        if 'consonantLength' in mora:
-            c = mora.get('consonantLength', 0)
-            mora['vowelLength'] = self.average_length - c
-        else:
-            mora['vowelLength'] = self.average_length
-        c = mora.get('consonantLength', 0)
-        v = mora.get('vowelLength', 0)
-        print(c + v)
-
-    def pause_mora_handler(self, pause_mora):
-        self.mora_handler(pause_mora)
-
-    def generate_result(self, accent_phrases):
-        return accent_phrases
-
-
-with open(r'test.json.vvproj', 'r', encoding="utf-8") as f:
-    di = json.load(f)
-
-ap = di['talk']['audioItems']['89ea0174-6b44-44ac-b196-0882830238e1']['query']['accentPhrases']
-APDumper().run(ap)
-print(APMorasCounter().run(ap))
-APMoraLengthAdjuster().run(ap)
-APDumper().run(ap)
-
-# s = json.dumps(di, indent=2, ensure_ascii=False)
-# print(s)
-# with open('test.json', 'w', encoding="utf-8") as f:
-#     json.dump(di, f, indent=2, ensure_ascii=False)
+    
+    
