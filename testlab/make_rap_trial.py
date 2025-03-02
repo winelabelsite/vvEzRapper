f81f3ad76e95393560ded051586a3b00e684d81b
nuumanP <wlsjapan@yahoo.co.jp>
2025-03-02 09:24:44 +0900

    実験その１終了。voicevox.pyってファイル名はおこがましいので変更。

--- testlab/make_rap_trial.py
+++ testlab/make_rap_trial.py
@@ -1,4 +1,3 @@
-import json
 import AccentPhraseApps as APA
 import wavhandler as WH
 import accessEngine as AE
@@ -10,12 +9,13 @@ def calc_ratio(bpm, wave_length, moras_count):
     spb_given = 60 / bpm / 4
     spb_before = wave_length / moras_count
     ratio = spb_given / spb_before
-    print(f'spb_given = {spb_given:.6f}, spb_before = {spb_before:.6f}, ratio = {ratio:.6f}')
+    # print(f'ratio = {ratio:.6f}')
     return ratio
 
 def calc_expected_length(bpm, moras_count):
     """bpmとモーラ数から、期待される音声の長さを計算する"""
     expected_length = 60 * moras_count / bpm / 4
+    print(f'expected_length = {expected_length:.6f}')
     return expected_length
 
 def make_sample(text, is_kana=False, wavefilename=None):
@@ -28,10 +28,11 @@ def make_sample(text, is_kana=False, wavefilename=None):
             query['accentPhrases'] = accent_phrases
             query['kana'] = text
         wave = ae.synthesis(query)
-        length_before = wh.get_length(wave)
+        length = wh.get_length(wave)
         if wavefilename is not None:
             wh.write(wavefilename, wave)
-        return length_before, query 
+        # print(f'make_sample length = {length}')
+        return length, query 
 
 def set_average_length(query, wavefilename=None):
         """クエリの各モーラの長さを平均化し、そのクエリを返す"""
@@ -41,10 +42,9 @@ def set_average_length(query, wavefilename=None):
         APA.APMoraLengthAdjuster(average_length).run(query)
         APA.APDumper().run(query)
         wave = ae.synthesis(query)
-        length = wh.get_length(wave)
         if wavefilename is not None:
             wh.write(wavefilename, wave)
-        return length, query
+        return query
 
 def make_wavefile_from_query(query, wavefilename=None):
     """クエリから音声波形を生成し、その長さを返す"""
@@ -54,6 +54,7 @@ def make_wavefile_from_query(query, wavefilename=None):
     length = wh.get_length(wave)
     if wavefilename is not None:
         wh.write(wavefilename, wave)
+    # print(f'make_wavefile_from_query length = {length}')
     return length
 
 
@@ -69,22 +70,14 @@ def trial_algo01(text):
     query['prePhonemeLength'] = 0.0
     query['postPhonemeLength'] = 0.0
     query["speedScale"] = 1 / ratio
-    query['outputSamplingRate'] = 48000
+    query['outputSamplingRate'] = 24000
 
     # waveファイルを作成
     length_after = make_wavefile_from_query(query, wavefilename='algo01_after.wav')
     expected_length = calc_expected_length(BPM, moras_count)
     difference = length_after - expected_length
-    # これ一つにまとめて使いまわしたいよね。
-    # print(f'moras_count = {moras_count}')   # queryから出せる
-    # print(f'length_before = {length_before:.6f}') # make_sampleから出せる
-    # print(f'ratio = {ratio:.6f}') # 上二つがわかれば出せる 
-    # print(f'length_after = {length_after:.6f}')   # 長さいじった後なら出せる
-    # print(f'expected_length = {expected_length:.6f}') # すぐ出せる
-    # print(f'difference = {difference}')   # すぐ出せる
-    # print(json.dumps(query, indent=2, ensure_ascii=False))    # 気になったら表示
-    print(expected_length / difference)
-    pass
+    print(f'difference = {difference:.9f}')
+    return query
 
 
 if __name__ == "__main__":
@@ -102,7 +95,8 @@ if __name__ == "__main__":
 
     texts = texts02 + texts01
 
-    for text in texts[-1:]:
+    for text in texts:
+        print(f'text = {text}, length = {len(text)}')
         trial_algo01(text)
 
     
